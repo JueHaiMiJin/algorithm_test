033dd492d1795d7510cac44e4d62cfd139fd2b79
JueHaiMiJin <49298034+JueHaiMiJin@users.noreply.github.com>
2021-01-22 14:28:13 +0800

    线程池文件整理&添加main文件

---
+++ 线程池/main.cpp
@@ -0,0 +1,17 @@
+#include "mutex/mutex.h"
+#include "task/task.h"
+
+int main(int argc, char const *argv[])
+{
+    //初始化线程池
+    
+
+
+    //循环
+    while (1)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
