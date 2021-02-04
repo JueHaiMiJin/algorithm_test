0c2c3517bd47efa7587e3eb2dfe9aa51016e9a0b
JueHaiMiJin <49298034+JueHaiMiJin@users.noreply.github.com>
2021-02-04 16:52:46 +0800

    修改斐波那契

--- 斐波那契/feibonaqi.cpp
+++ 斐波那契/feibonaqi.cpp
@@ -37,8 +37,9 @@ public:
     int fib(int n)
     {
         if (n < 2)
+        {
             return n;
-
+        }
         vector<vector<int>> q{{1, 1}, {1, 0}};
         vector<vector<int>> res = matrix_pow(q, n - 1);
         return res[0][0];
@@ -47,22 +48,21 @@ public:
     vector<vector<int>> matrix_pow(vector<vector<int>> &a, int n)
     {
         vector<vector<int>> ret{{1, 0}, {0, 1}};
-        while (n)
+        while (n > 0)
         {
             if (n & 1)
             {
-                ret = Mulitp(ret, a);
+                ret = matrix_multiply(ret, a);
             }
-
             n >>= 1;
-            Mulitp(ret, a);
+            a = matrix_multiply(a, a);
         }
         return ret;
     }
 
-    vector<vector<int>> Mulitp(vector<vector<int>> &a, vector<vector<int>> &b)
+    vector<vector<int>> matrix_multiply(vector<vector<int>> &a, vector<vector<int>> &b)
     {
-        vector<vector<int>> c = {{0, 0}, {0, 0}};
+        vector<vector<int>> c{{0, 0}, {0, 0}};
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 2; j++)
